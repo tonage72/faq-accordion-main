346d491bf54b02ee7d66b4f8c69fca39a7b55284
David Fleming <tonage72@gmail.com>
2024-04-08 19:16:07 -0400

    Need to limit pull down to just plus

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const plus = document.querySelectorAll('.plus');
+const questionAnswer = document.querySelectorAll('.question-answer')
+
+for (i=0; i < questionAnswer.length; i++) {
+	questionAnswer[i].addEventListener('click', function() {
+
+		if (this.classList.contains('active')) {
+			for (i = 0; i < questionAnswer.length; i++) {
+				questionAnswer[i].classList.remove('active');
+			}
+		} else {
+			for (i = 0; i < questionAnswer.length; i++) {
+				questionAnswer[i].classList.remove('active');
+			}
+			this.classList.add('active');
+		}
+	})
+}
